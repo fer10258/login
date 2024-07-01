69e264a300f8b0b3fe3d78efaee08f5b2cf26c01
hata <fernandohata@hotmail.com>
2024-07-01 13:21:46 -0300

    V4 - Refactory All

--- app/src/test/java/com/hata/login/ExampleUnitTest.java
+++ app/src/test/java/com/hata/login/ExampleUnitTest.java
@@ -1,25 +1,39 @@
-package com.hata.login;
-
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.hata.login.Presentation.HomeActivity.HomeContract;
 import com.hata.login.Presentation.HomeActivity.HomePresenter;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({FirebaseAuth.class, FirebaseAuth.class, HomePresenter.class})
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
 public class ExampleUnitTest {
-    HomePresenter presenter;
-    HomePresenter view;
+
+    @Mock
+    HomeContract.View mockView;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        PowerMockito.mockStatic(Process.class);
+        when(Process.myPid()).thenReturn(1234);
+    }
+
     @Test
     public void checkStringAdd() {
-        presenter = new HomePresenter((HomeContract.View) view);
+        HomePresenter presenter = new HomePresenter(mockView);
         String newString = presenter.newText("Pitanga");
         assertEquals("Pitanga ", newString);
     }
-
 }
